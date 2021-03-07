0a65daab2ad2888bfc04193836dfbd99bc673869
ZvalsoraY <yazamail@yandex.ru>
2021-03-07 12:13:05 +0300

    Add project files.

---
+++ Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPreparationToExcel
+{
+    static class Converter
+    {
+        //string fileName = $"20tfiEMAT_20_ver_st3_WT10_sens3p5";
+        static public List<string> list = new List<string>();
+        
+        static public void consoleWriteCheck(List<string> list)
+        {
+            foreach (var s in list)
+                Console.Write(" " + s);
+            //Console.Write(s);
+            Console.WriteLine();
+        }
+        
+        static void consoleWriteCheck(List<List<double>> writingArray)
+        {
+            foreach (var item in writingArray)
+            {
+                foreach (var s in item)
+                    Console.Write(" " + s);
+                //Console.Write(s);
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataPreparationToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog fbd= new FolderBrowserDialog();
+            //fbd.ShowDialog();
+            //if(fbd.ShowDialog() == DialogResult.OK)
+            //{
+            //    string[] files_list = 
+            //}
+            OpenFileDialog dlg = new OpenFileDialog() 
+            {
+                Multiselect = true,
+                Title = "Select files",
+            };
+            //Filter by output files
+            dlg.Filter = "Output files|*.output";
+            dlg.ShowDialog();
+
+            if (dlg.FileName == String.Empty)
+                return;
+            string[] files_names = dlg.FileNames;
+            //List<string> names_list = dlg.FileNames.ToList();
+            //string[] files_names = System.IO.
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(files_names);
+            Converter.list.Clear();
+            Converter.list.AddRange(files_names);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Converter.consoleWriteCheck(Converter.list);
+        }
+    }
+}
